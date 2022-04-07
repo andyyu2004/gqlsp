ac055f121d85451c601e703d321d46ca4ee92589
Andy Yu <andyyu2004@gmail.com>
2022-04-07 21:33:31 +1200

    Move rust crates into nested subdir and use json logging when not atty

--- src/gqls/src/bin/gqls.rs
+++ src/gqls/src/bin/gqls.rs
@@ -4,7 +4,13 @@ use tower_lsp::{LspService, Server};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::FmtSubscriber::builder().with_writer(std::io::stderr).init();
+    let builder = tracing_subscriber::fmt().with_writer(std::io::stderr);
+    if atty::is(atty::Stream::Stderr) {
+        builder.init();
+    } else {
+        builder.json().init();
+    }
+    tracing::error!("hello");
     let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
     let (service, socket) = LspService::new(Gqls::new);
     Server::new(stdin, stdout, socket).serve(service).await;
