27c0f6a4cd0a7b112bff205938ff14d44050a528
Andy Yu <andyyu2004@gmail.com>
2022-04-15 22:20:36 +1200

    add simple expect test for body lowering

--- src/gqls-ir/src/lower/tests.rs
+++ src/gqls-ir/src/lower/tests.rs
@@ -1,3 +1,4 @@
+use expect_test::expect;
 use vfs::Vfs;
 
 use crate::tests::{idx, setup, TestDB};
@@ -20,5 +21,42 @@ fn test_lower_item_body() {
     });
 
     let body = db.item_body(foo, idx!(0));
-    dbg!(body);
+    expect![[r#"
+        TypeDefinition(
+            TypeDefinitionBody {
+                fields: Fields {
+                    fields: Arena {
+                        len: 6,
+                        data: [
+                            Field {
+                                name: foo,
+                                ty: Int,
+                            },
+                            Field {
+                                name: list,
+                                ty: [Int],
+                            },
+                            Field {
+                                name: nonNull,
+                                ty: Int!,
+                            },
+                            Field {
+                                name: nonNullList,
+                                ty: [Int!]!,
+                            },
+                            Field {
+                                name: a,
+                                ty: [Int!],
+                            },
+                            Field {
+                                name: b,
+                                ty: [Int]!,
+                            },
+                        ],
+                    },
+                },
+            },
+        )
+    "#]]
+    .assert_debug_eq(&body);
 }
