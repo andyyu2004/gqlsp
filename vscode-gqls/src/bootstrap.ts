335234e7efe0939bc0e4997f46101f8df43ccf9b
Andy Yu <andyyu2004@gmail.com>
2022-05-04 23:34:02 +1200

    update releases

--- vscode-gqls/src/bootstrap.ts
+++ vscode-gqls/src/bootstrap.ts
@@ -6,7 +6,7 @@ import { dirname, join } from "path";
 import { createGunzip } from "zlib";
 import assert = require("assert");
 
-const GQLS_VERSION = "v0.0.3";
+const GQLS_VERSION = "v0.0.4";
 
 export async function bootstrap(
   context: vscode.ExtensionContext,
